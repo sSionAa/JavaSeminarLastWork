3f8026e721766ff763be6f5d6e05714757c774a5
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-03-20 20:21:01 +0300

    Create Validate.java

---
+++ Validate.java
@@ -0,0 +1,18 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Validate {
+    final static String DATE_FORMAT = "dd.MM.yyyy";
+
+    public static boolean isDateValid(String date) {
+        try {
+            DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+            df.setLenient(false);
+            df.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+}
